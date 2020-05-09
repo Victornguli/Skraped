dccc0cc443eb187cbcc75034845002411ded9b43
Victor Joseph <victornjoseph@gmail.com>
2020-05-09 22:52:42 +0300

    first commit

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+from skraper import __version__ as version
+
+description = 'CLI tool for scraping and aggregating Kenyan tech job posted on various job boards'
+url = 'https://github.com/Victornguli/Skraped'
+requires = [
+	'pytest>=5.4.2'
+]
+
+with open("README.md", 'r') as f:
+	long_description = f.read()
+
+setup(
+	name = 'Skraped',
+	version = version,
+	description = description,
+	long_description = long_description,
+	long_description_content_type = 'text/markdown',
+	author = 'Victor Nguli Joseph',
+	author_email = 'victornjoseph@gmail.com',
+	url = url,
+	license = 'MIT License',
+	python_requires = '>=3.6.0',
+	install_requires = requires,
+	packages = find_packages(exclude = 'tests'),
+	include_package_data = True,
+	entry_points = {'console_scripts': ['skraper=skraper.__main__:main']}
+)
---
+++ skraper/__main__.py
@@ -0,0 +1,7 @@
+
+def main():
+	print('Main Fn in working')
+
+
+if __name__ == "__main__":
+	main()
