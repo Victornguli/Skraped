5779f73833a4a7604c2aeb66aefd30e696c1529a
Victor Joseph <victornjoseph@gmail.com>
2020-05-25 22:18:54 +0300

    conf validate tests fix

--- tests/test_validate_config.py
+++ tests/test_validate_config.py
@@ -1,22 +1,27 @@
 import argparse
 import pytest
+from unittest import TestCase
 from skraped.config.validate_config import validate_conf, ConfigError, validate_output_path, validate_sources
 
 
-def test_validate_config():
-    """Tests for config validation method"""
-    conf = {
-        'output_path': '', 'sources': ['Linkedin', 'BrighterMonday', 'MyJobMag'],
-        'keywords': 'Software Developer'}
-    with pytest.raises(ConfigError):
-        validate_conf(conf)
+class TestValidateConfig(TestCase):
+    def setUp(self):
+        self.valid_config = {
+            'output_path': 'data', 'sources': ['Glassdoor', 'BrighterMonday'],
+            'keywords': 'Software Developer'}
+        self.invalid_config = {
+            'output_path': 'data', 'sources': ['Glassdoor', 'BrighterMonday', 'RandomJobSite'],
+            'keywords': 'Software Developer'}
 
+    def test_validate_config(self):
+        """Tests for config validation method"""
+        with pytest.raises(ConfigError):
+            validate_conf(self.invalid_config)
 
-def test_validate_path_fail():
-    with pytest.raises(ConfigError):
-        validate_output_path('')
+    def test_validate_path_fail(self):
+        with pytest.raises(ConfigError):
+            validate_output_path('', self.valid_config)
 
-
-def test_validate_sources_fail():
-    with pytest.raises(ConfigError):
-        validate_sources(['Linkedin', 'RandomJobSite'])
+    def test_validate_sources_fail(self):
+        with pytest.raises(ConfigError):
+            validate_sources(['Linkedin', 'RandomJobSite'], self.valid_config)
